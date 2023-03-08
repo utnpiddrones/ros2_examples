c05c2eeb7dc080abdaf9906b589af09913830be7
ncotti <ncotti@frba.utn.edu.ar>
2023-03-08 17:14:09 -0300

    added interactive terminal in first steps

--- first_steps/exec.sh
+++ first_steps/exec.sh
@@ -5,6 +5,7 @@ echo "What to do?"
 echo "1. Run turtlesim"
 echo "2. Control turtlesim"
 echo "3. Open rqt."
+echo "4. Open terminal."
 echo "q. Exit"
 
 read -p "Select option: " -r -n 1 option
@@ -16,6 +17,8 @@ case $option in
     2) command="ros2 run turtlesim turtle_teleop_key";;
 
     3) command="rqt";;
+
+    4) command="/bin/bash";;
 
     *) {
         echo "Exiting..."
